248b006879fb4d7505679d9ab5d65dafe6bbb819
Khaudd7ff <138776907+Khaudd7ff@users.noreply.github.com>
2024-10-28 11:16:20 +1400

    Update Tp01.c
    matrice

--- Tp01.c
+++ Tp01.c
@@ -1,1 +1,75 @@
+        #include<stdio.h>
+        #include<string.h>
 
+        int main(){
+
+         char M[3][3]={{'1','2','3'},{'a','b','c'},{'w','9','0'}};
+
+        int i,j,n,s;
+
+
+        for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+                     printf("%c",M[i][j]);
+            printf("\t");
+            }
+             printf("\n");
+            }
+
+             printf("\n");
+              printf("\n");
+              printf("\n");
+
+
+        for(i=0;i<3;i++){
+
+            for(j=0;j<3;j++){
+
+                    if(i==j){
+
+
+
+                        printf("%c",M[i][j]);
+                        printf("\t");}
+
+
+            }}
+
+            printf("\n");
+             printf("\n");
+              printf("\n");
+               printf("\n");
+
+            for (j=0;j<3;j++){
+
+                for(i=j+1;i<3;i++){
+
+
+                    s=M[i][j];
+                    M[i][j]=M[j][i];
+                    M[j][i]=s;
+
+
+                }
+
+            for (i = 0; i < 3; i++) {
+                    for (j = 0; j < 3; j++) {
+                        printf("%c\t", M[i][j]);
+                    }
+                    printf("\n");
+                }
+
+            }
+
+
+            printf("\n");
+
+
+
+
+
+
+
+        return 0;
+}
